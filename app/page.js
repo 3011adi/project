bbd806b2c3ccd55c9b121060e3ec084a1a48d9e6
Adi Naik <161313416+3011adi@users.noreply.github.com>
2024-07-03 23:08:42 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,71 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+    <div >
+       <div  className="flex">
+        <div className="w-64"></div>
+     <div className="grid grid-cols-3 gap-4 grid-rows-7 min-h-screen">
+      <div className=" col-span-2 row-span-2 text-8xl ">bring the future of the  technology companies</div>
+      <div className=" bg-slate-300 flex">
+       
+         <div className="bg-blue-950 w-64 rounded-lg" >t</div>
+          <div className=" rounded-lg  w-64  text-6xl py-10 text-center text-blue-800">38+</div>
+        
+      </div>
+      <div className="bg-slate-400 text-gray-600 rounded-lg text-md">.  jhdhwd wdwkeudewud wekdhwedhwud edihweidhweidwdi
+      hhddd qdiqwdgqwdgqwdg qwdhqiudqwdgqwyidgy</div>
+      <div className="bg-blue-950  col-span-2 row-span-3 rounded-lg ">3</div>
+      <div className="bg-blue-950 row-span-4 rounded-lg relative">
+
+  <div className="bg-white rounded-xl w-32 text-2xl absolute bottom-4 right-4">. learnmore</div>
+       
+       </div>
+      <div className="bg-blue-500 rounded-lg text-white  ">
+            <div className="bg-blue-950 w-16 h-16 "> s</div>
+          <h1 className="text-2xl px-4 ">great tech ecosystem</h1>
+          <h1 className="px-5">hdbwebfwfwfbe</h1></div>
+      <div className="bg-slate-300 rounded-lg "><h1 className="text-6xl text-blue-700">120+</h1>
+        <h1 className="text-2xl">affiliate company</h1>
+        <h1>djdfjnwkefnjkfn fwef</h1></div>
+     
+     
+     </div>
+
+     
+     
+     <div className="w-64"></div>
+     </div>
+      
+      
+        
+      
+      <div  className="flex">
+        <div className="w-64"></div>
+        <div className="grid grid-cols-5  grid-rows-5 gap-4 min-h-screen">
+        <div className=" col-span-2 row-span-2"><h className="text-5xl font-semibold" >collborative to buid the innovative tech ecosystem</h>
+        <h1 className="text-lg py-3">knenddnweuneu ejniwiwejwe wjniowcnowcnwe wnciowcnoiwncwnc cencme ejnnwenw ejniwnwi</h1></div>
+        <div className="bg-blue-950 rounded-lg col-span-2 row-span-4">2</div>
+        <div className=" row-span-2 relative"> <h1 className="text-3xl">collaboration is key of sucess</h1>
+         <h1 className="py-4">jefjwffufufbufbuf efnwfnwofnewofnew eiofniownfoiwnfowfnw</h1>
+           <div className="text-white bg-blue-600 w-32 rounded-xl  absolute bottom-4 right-4"> <h1 className="p-3"> moreaboutus</h1></div></div>
+        <div className="bg-slate-600 rounded-lg col-span-2 flex "><div className="bg-red-900 w-32"></div>
+        <div><h1 className="text-3xl px-4 py-4 font-semibold text-white">fund with expert sratergies</h1>
+        <h1 className="text-xl px-4 py-4 ">jeedundunwud nwduwdw</h1></div></div>
+        <div className="bg-blue-950 row-span-2 rounded-lg">5</div>
+        <div className="bg-slate-600  rounded-lg col-span-2 flex "><div className="bg-red-900 w-32"></div>
+        <div><h1 className="text-3xl px-4 py-4 font-semibold text-white">fund with expert sratergies</h1>
+        <h1 className="text-xl px-4 py-4 ">jeedundunwud nwduwdw</h1></div></div>
+       
+      </div>
+
+     
+     
+     <div className="w-64"></div>
+     </div>
+
+    </div>
+  
+  );
+}
